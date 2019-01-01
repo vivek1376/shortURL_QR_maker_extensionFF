a9c32832d6fa276aa705b3a2b662e6003dbb93a7
Vivek Mehra <vivek.1376@gmail.com>
2019-01-01 03:13:37 -0500

    - ui looks ready, mostly doesn't break in case of malformed or longer strings, but needs more testing. - tested for error cases both for is.gd & qr fetch failure; displaying just a 'Error !' string to user.

--- popup/choose_beast.js
+++ popup/choose_beast.js
@@ -1,14 +1,19 @@
-
-
-
 /**
  * There was an error executing the script.
  * Display the popup's error message, and hide the normal UI.
  */
 function reportExecuteScriptError(error) {
-  document.querySelector("#popup-content").classList.add("hidden");
-  document.querySelector("#error-content").classList.remove("hidden");
-  console.error(`Failed to execute beastify content script: ${error.message}`);
+    printError();
+    // document.querySelector("#popup-content").classList.add("hidden");
+    // document.querySelector("#error-content").classList.remove("hidden");
+    console.error(`Failed to execute content script: ${error.message}`);
+}
+
+
+function printError(errStr) {
+    errStr = errStr || 'Error !';
+
+    document.querySelector('div#footer p.status').innerText = errStr;
 }
 
 /**
@@ -17,110 +22,155 @@ function reportExecuteScriptError(error) {
  * If we couldn't inject the script, handle the error.
  */
 browser.tabs.executeScript({file: "/content_scripts/beastify.js"})
-  .catch(reportExecuteScriptError);
+    .catch(reportExecuteScriptError);
 
 // for communication
 browser.runtime.onMessage.addListener(notify);
 
 function notify(message) {
 
-  var isgdUrl = "";
+    var isgdUrl_str = "";
 
-  console.log("In message()");
-  // document.getElementById("urlinfo").innerText = message.url;
+    console.log("In message()");
+    // document.getElementById("urlinfo").innerText = message.url;
 
-  // var url = 'https://www.pbs.org/video/great-conversations-great-conversations-siddhartha-mukherjee';
-  var url_encoded = encodeURIComponent(message.url);
+    // var url = 'https://www.pbs.org/video/great-conversations-great-conversations-siddhartha-mukherjee';
+    var url_encoded = encodeURIComponent(message.url);
 
-  var apiUrl = 'https://is.gd/create.php?format=json&url=' + url_encoded;
-  console.log("URL: " + apiUrl);
+    var apiUrl = 'https://is.gd/create.php?format=json&url=' + url_encoded;
+    console.log("URL: " + apiUrl);
 
-  var xhttp = new XMLHttpRequest();
+    var xhttp = new XMLHttpRequest();
 
-  if (!xhttp) {
-    return false;
-  }
-
-  xhttp.onreadystatechange = function() {
-    console.log("http response: " + this.status);
-
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-      console.log(this.response);
-      var resp = JSON.parse(this.responseText);
-      if (resp.hasOwnProperty('shorturl')) {
-        isgdUrl = resp.shorturl;
-        var isgdUrlNoHttp = isgdUrl.substring(8);
-
-        document.getElementById("urlinfo").innerText = isgdUrlNoHttp;
-
-
-        //
-        // var xhttp2 = new XMLHttpRequest();
-        //
-        // if (!xhttp2) {
-        //   return false;
-        // }
-        //
-        //
-        // xhttp2.onload = function(ovent) {
-        //   // var blob = new Blob([this.response], {type: 'image/png'});
-        //   // var blob = xhttp2.response;
-        //
-        //   console.log("onload function");
-        //
-        //
-        //   var imageEl = document.getElementById("qrimg");
-        //   imageEl.src = this.
-        //
-        //   //for downlaoding
-        //   // var a = document.createElement("a");
-        //   // a.style = "display: none";
-        //   // document.body.appendChild(a);
-        //   // var url = window.URL.createObjectURL(blob);
-        //   //
-        //   // a.href = url;
-        //   // a.download = 'myqrimage.png';
-        //   //
-        //   // a.click();
-        //   //
-        //   // window.URL.revokeObjectURL(url);
-        // };
-        //
-        url2 = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chld=M|0&chl='
-          + encodeURIComponent(isgdUrl);
-
-        console.log(url2);
-        // xhttp2.open("GET", url2, true);
-        // xhttp2.setRequestHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-        // xhttp2.responseType = 'arraybuffer'; //'blob';
-        // xhttp2.send();
-
-
-        var image = document.getElementById("qrimg");
-        var downloadingImage = new Image();
-
-        downloadingImage.onload = function(){
-          image.src = this.src;
-
-        };
-
-        downloadingImage.src =  url2;
-      }
-    } else {
-      console.log("error");
-      console.log(this.response);
+    if (!xhttp) {
+        return false;
     }
-  };
+
+    // xhttp.onreadystatechange = function() {
+    xhttp.onload = function() {
+        var resp = undefined;
+        var isError = false;
+
+        console.log("http response: " + this.status);
+
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            console.log("1: " + this.response);
+            console.log("2: " + this.responseText);
+
+            try {
+                var resp = JSON.parse(this.responseText);
+            } catch (e) {
+                isError = true;
+            }
+
+            if (typeof resp !== 'undefined' && resp.hasOwnProperty('shorturl')) {
+                document.querySelector('div#footer p.status').innerText = 'loading qr code...';
+                isgdUrl_str = resp.shorturl;
+                var isgdUrl_str_formatted = isgdUrl_str.substring(8);
+
+                document.getElementById("urlinfo").innerText = isgdUrl_str_formatted;
+
+                url_qr = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chld=M|0&chl='
+                    + encodeURIComponent(isgdUrl_str);
+
+                console.log(url_qr);
+
+                var image = document.getElementById("qrimg");
+                var downloadingImage = new Image();
+
+                downloadingImage.onload = function(){
+                    image.src = this.src;
+                    document.querySelector('div#footer p.status').innerText = '';
+                };
+
+                downloadingImage.onerror = function() {
+                    console.log("image error.");
+                    printError();
+                };
+
+                downloadingImage.src =  url_qr;
 
 
-  xhttp.open("GET", apiUrl, true);
-  xhttp.setRequestHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-  xhttp.send();
+                function copy() {
+                    navigator.clipboard.writeText(isgdUrl_str).then(function() {
+                        /* clipboard successfully set */
+                        console.log("success");
+                    }, function() {
+                        /* clipboard write failed */
+                        console.log("error");
+                    });
+                }
+
+                document.querySelector('input#copybtn').addEventListener("click", copy);
+
+            } else if (typeof resp !== 'undefined' && resp.hasOwnProperty('errorcode')) {
+                isError = true;
+                console.log("error response");
+            }
+        } else {
+            isError = true;
+            console.log("error");
+        }
+
+        if (isError) {
+            printError();
+        }
+    };
 
 
+    var oldStStr = '';
 
 
+    var likeIcon_obj = document.querySelector('div#footer div.about a');
 
+    likeIcon_obj.addEventListener("mouseover", function () {
+        console.log("mouseover");
+        oldStStr = document.querySelector('div#footer p.status').innerText;
+        document.querySelector('div#footer p.status').innerText = 'GOTO homepage';
+    });
+
+    likeIcon_obj.addEventListener("mouseout", function () {
+        console.log("mouseout");
+        document.querySelector('div#footer p.status').innerText = oldStStr;
+    });
+
+
+    var isCopyClicked = false;
+    var copyStatusTimeout = undefined;
+
+    var copyIcon_obj = document.querySelector('input#copybtn');
+
+    copyIcon_obj.addEventListener("mouseover", function () {
+        if (isCopyClicked == true) {
+            isCopyClicked = false;
+        } else {
+            oldStStr = document.querySelector('div#footer p.status').innerText;
+            document.querySelector('div#footer p.status').innerText = 'Click to copy url';
+        }
+    });
+
+    copyIcon_obj.addEventListener("mouseout", function () {
+        console.log("mouseout");
+
+        if (isCopyClicked == true) {
+            isCopyClicked = setTimeout(function () {
+                document.querySelector('div#footer p.status').innerText = oldStStr;
+            }, 1800);
+        } else {
+            document.querySelector('div#footer p.status').innerText = oldStStr;
+        }
+    });
+
+    copyIcon_obj.addEventListener("click", function () {
+        isCopyClicked = true;
+        console.log("clicked");
+        document.querySelector('div#footer p.status').innerText = 'copied !';
+    });
+
+    xhttp.open("GET", apiUrl, true);
+    xhttp.setRequestHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    xhttp.overrideMimeType("application/json");
+    xhttp.send();
 }
 
 
