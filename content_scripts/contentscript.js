e671594db74fb7526877510db3de11cb51131780
Vivek Mehra <vivek.1376@gmail.com>
2019-01-03 18:14:52 -0500

    - added context-menu copy feature   - made is.gd api call common for popup & context-menu - cleaned code a bit

--- content_scripts/contentscript.js
+++ content_scripts/contentscript.js
@@ -1,21 +1,74 @@
 (function() {
-    /**
-     * Check and set a global guard variable.
+    /* Check and set a global guard variable.
      * If this content script is injected into the same page again,
-     * it will do nothing next time.
-     */
+     * it will do nothing next time. */
     if (typeof window.isContentScriptRunning !== "undefined" && window.isContentScriptRunning == 1) {
         console.log("returning.");
         return;
     }
 
-
-    console.log("not returning.");
-
     window.isContentScriptRunning = 1;
 
-    browser.runtime.sendMessage({"url": window.location.href});
-    console.log("sent");
+    if (typeof window.isgdshortURL !== "undefined") {
+        console.log("found isgd cached url. returning.");
+        console.log(window.isgdshortURL);
+
+        browser.runtime.sendMessage({"isgdurl": window.isgdshortURL});
+        window.isContentScriptRunning = 0;
+
+        return;
+    }
+
+    console.log("inside content script");
+
+    var pageURL = window.location.href;
+
+    var url_encoded = encodeURIComponent(pageURL);
+
+    var apiUrl = 'https://is.gd/create.php?format=json&url=' + url_encoded;
+    console.log("URL: " + apiUrl);
+
+    var xhttp = new XMLHttpRequest();
+
+    if (!xhttp) {
+        window.isContentScriptRunning = 0;
+        return;
+    }
+
+    xhttp.onload = function() {
+        var resp = undefined;
+        var isError = false;
+
+        console.log("http response: " + this.status);
+
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            // console.log("2: " + this.responseText);
+
+            try {
+                var resp = JSON.parse(this.responseText);
+            } catch (e) {
+                isError = true;
+            }
+
+            if (typeof resp !== 'undefined' && resp.hasOwnProperty('shorturl')) {
+                window.isgdshortURL = resp.shorturl;
+                browser.runtime.sendMessage({"isgdurl": window.isgdshortURL});
+            } else {
+                if (resp.hasOwnProperty('errorcode')) {
+                    browser.runtime.sendMessage({"error": resp.errorcode});
+                } else {
+                    browser.runtime.sendMessage({"error": "error"});
+                }
+            }
+        } else {
+            console.log("in ELSE part.");  //???
+        }
+    };
+
+    xhttp.open("GET", apiUrl, true);
+    xhttp.setRequestHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    xhttp.overrideMimeType("application/json");
+    xhttp.send();
 
     window.isContentScriptRunning = 0;
 })();
--- popup/popup.js
+++ popup/popup.js
@@ -5,13 +5,12 @@
 // document.querySelector('div#aboutBox').classList.add('hidden');
 
 
-custom_var = "custom_val";
+// custom_var = "custom_val";
+
+var isErrorFlag = false;
 
 function reportExecuteScriptError(error) {
     printError();
-    // document.querySelector("#popup-content").classList.add("hidden");
-    // document.querySelector("#error-content").classList.remove("hidden");
-    console.error(`Failed to execute content script: ${error.message}`);
 }
 
 
@@ -21,11 +20,9 @@ function printError(errStr) {
     document.querySelector('div#footer p.status').innerText = errStr;
 }
 
-/**
- * When the popup loads, inject a content script into the active tab,
+/* When the popup loads, inject a content script into the active tab,
  * and add a click handler.
- * If we couldn't inject the script, handle the error.
- */
+ * If we couldn't inject the script, handle the error. */
 browser.tabs.executeScript({file: "/content_scripts/contentscript.js"})
     .catch(reportExecuteScriptError);
 
@@ -34,117 +31,57 @@ browser.runtime.onMessage.addListener(notify);
 
 function notify(message) {
 
-    var isgdUrl_str = '';
+    console.log("got message" + JSON.stringify(message));
 
-    console.log("In message()");
-
-    // document.getElementById("urlinfo").innerText = message.url;
-
-    // var url = 'https://www.pbs.org/video/great-conversations-great-conversations-siddhartha-mukherjee';
-    var url_encoded = encodeURIComponent(message.url);
-
-    var apiUrl = 'https://is.gd/create.php?format=json&url=' + url_encoded;
-    console.log("URL: " + apiUrl);
-
-    var xhttp = new XMLHttpRequest();
-
-    if (!xhttp) {
-        return false;
+    if (message.hasOwnProperty('error')) {
+        printError();
+        return;
     }
 
-    // xhttp.onreadystatechange = function() {
-    xhttp.onload = function() {
-        var resp = undefined;
-        var isError = false;
 
-        console.log("http response: " + this.status);
+    var isgdUrl_str = message.isgdurl;
 
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            console.log("1: " + this.response);
-            console.log("2: " + this.responseText);
+    console.log("isgdurl: " + isgdUrl_str);
 
-            try {
-                var resp = JSON.parse(this.responseText);
-            } catch (e) {
-                isError = true;
-            }
+    var isgdUrl_str_formatted = isgdUrl_str.substring(8);
 
-            if (typeof resp !== 'undefined' && resp.hasOwnProperty('shorturl')) {
-                document.querySelector('div#footer p.status').innerText = 'loading qr code...';
-                isgdUrl_str = resp.shorturl;
-                var isgdUrl_str_formatted = isgdUrl_str.substring(8);
+    document.getElementById("urlinfo").innerText = isgdUrl_str_formatted;
 
-                document.getElementById("urlinfo").innerText = isgdUrl_str_formatted;
+    url_qr = 'https://chart.googleapis.com/chart?cht=qr&chs=250x250&chld=M|0&chl='
+        + encodeURIComponent(isgdUrl_str);
 
-                url_qr = 'https://chart.googleapis.com/chart?cht=qr&chs=250x250&chld=M|0&chl='
-                    + encodeURIComponent(isgdUrl_str);
+    console.log(url_qr);
 
-                console.log(url_qr);
+    var image = document.getElementById("qrimg");
+    var downloadingImage = new Image();
 
-                console.log("global var: " + window.myglobalvar);
-
-                var image = document.getElementById("qrimg");
-                var downloadingImage = new Image();
-
-                downloadingImage.onload = function(){
-                    image.src = this.src;
-                    document.querySelector('div#footer p.status').innerText = '';
-                };
-
-                downloadingImage.onerror = function() {
-                    console.log("image error.");
-                    printError();
-                };
-
-                downloadingImage.src =  url_qr;
-
-
-                function copy() {
-                    navigator.clipboard.writeText(isgdUrl_str).then(function() {
-                        /* clipboard successfully set */
-                        console.log("success");
-                    }, function() {
-                        /* clipboard write failed */
-                        console.log("error");
-                    });
-                }
-
-                document.querySelector('input#copybtn').addEventListener("click", copy);
-
-            } else if (typeof resp !== 'undefined' && resp.hasOwnProperty('errorcode')) {
-                isError = true;
-                console.log("error response");
-            }
-        } else {
-            isError = true;
-            console.log("error");
-        }
-
-        if (isError) {
-            printError();
-        }
+    downloadingImage.onload = function () {
+        image.src = this.src;
+        document.querySelector('div#footer p.status').innerText = '';
     };
 
+    downloadingImage.onerror = function () {
+        console.log("image error.");
+        printError();
+    };
 
-    var oldStStr = '';
+    downloadingImage.src = url_qr;
 
+    // copy button handler
+    function copy() {
+        console.log("url copied");
+        navigator.clipboard.writeText(isgdUrl_str).then(function () {
+            /* clipboard successfully set */
+            console.log("success");
+        }, function () {
+            /* clipboard write failed */
+            console.log("error");
+        });
+    }
 
-    var likeIcon_obj = document.querySelector('div#footer div.about a#likelink');
-
-    likeIcon_obj.addEventListener("mouseover", function () {
-        console.log("mouseover");
-        oldStStr = document.querySelector('div#footer p.status').innerText;
-        document.querySelector('div#footer p.status').innerText = 'GOTO homepage';
-    });
-
-    likeIcon_obj.addEventListener("mouseout", function () {
-        console.log("mouseout");
-        document.querySelector('div#footer p.status').innerText = oldStStr;
-    });
-
+    document.querySelector('input#copybtn').addEventListener("click", copy);
 
     var isCopyClicked = false;
-    var copyStatusTimeout = undefined;
 
     var copyIcon_obj = document.querySelector('input#copybtn');
 
@@ -152,7 +89,6 @@ function notify(message) {
         if (isCopyClicked == true) {
             isCopyClicked = false;
         } else {
-            oldStStr = document.querySelector('div#footer p.status').innerText;
             document.querySelector('div#footer p.status').innerText = 'Click to copy url';
         }
     });
@@ -162,10 +98,18 @@ function notify(message) {
 
         if (isCopyClicked == true) {
             isCopyClicked = setTimeout(function () {
-                document.querySelector('div#footer p.status').innerText = oldStStr;
+                if (isErrorFlag == true) {
+                    printError();
+                } else {
+                    document.querySelector('div#footer p.status').innerText = '';
+                }
             }, 1800);
         } else {
-            document.querySelector('div#footer p.status').innerText = oldStStr;
+            if (isErrorFlag == true) {
+                printError();
+            } else {
+                document.querySelector('div#footer p.status').innerText = '';
+            }
         }
     });
 
@@ -174,34 +118,46 @@ function notify(message) {
         console.log("clicked");
         document.querySelector('div#footer p.status').innerText = 'copied !';
     });
-
-
-    var isAboutDisplay = false;
-
-    var divmain_obj = document.querySelector('div#wrap');
-    var divAbtBox_obj = document.querySelector('div#aboutBox');
-    var divlike_obj = document.querySelector('div#footer div.about');
-
-    likeIcon_obj.addEventListener("click", function () {
-        divAbtBox_obj.classList.remove('hidden');
-        divmain_obj.classList.add('hidden');
-
-        divlike_obj.classList.add('hidden');
-    });
-
-    document.querySelector('a#closeabt').addEventListener("click", function () {
-        divmain_obj.classList.remove('hidden');
-        divlike_obj.classList.remove('hidden');
-        divAbtBox_obj.classList.add('hidden');
-    });
-
-
-    /* send api request to is.gd */
-    xhttp.open("GET", apiUrl, true);
-    xhttp.setRequestHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-    xhttp.overrideMimeType("application/json");
-    xhttp.send();
 }
+
+
+var likeIcon_obj = document.querySelector('div#footer div.about a#likelink');
+
+likeIcon_obj.addEventListener("mouseover", function () {
+    console.log("mouseover");
+    // oldStStr = document.querySelector('div#footer p.status').innerText;
+    document.querySelector('div#footer p.status').innerText = 'GOTO homepage';
+});
+
+likeIcon_obj.addEventListener("mouseout", function () {
+    console.log("mouseout");
+    if (isErrorFlag == true) {
+        printError();
+        // document.querySelector('div#footer p.status').innerText = oldStStr;
+    } else {
+        document.querySelector('div#footer p.status').innerText = '';
+    }
+});
+
+var isAboutDisplay = false;
+
+var divmain_obj = document.querySelector('div#wrap');
+var divAbtBox_obj = document.querySelector('div#aboutBox');
+var divlike_obj = document.querySelector('div#footer div.about');
+
+likeIcon_obj.addEventListener("click", function () {
+    divAbtBox_obj.classList.remove('hidden');
+    divmain_obj.classList.add('hidden');
+
+    divlike_obj.classList.add('hidden');
+});
+
+document.querySelector('a#closeabt').addEventListener("click", function () {
+    divmain_obj.classList.remove('hidden');
+    divlike_obj.classList.remove('hidden');
+    divAbtBox_obj.classList.add('hidden');
+});
+
 
 
 
